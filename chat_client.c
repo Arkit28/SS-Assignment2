0259cd86277d6967482cd1fc3df4919a754ecbab
Shashank-Vinoo <shashankvinoo.imp@gmail.com>
2025-12-08 12:07:21 +0000

    Skeleton code for chat client

--- chat_client.c
+++ chat_client.c
@@ -1,71 +1,104 @@
-
 #include <stdio.h>
 #include "udp.h"
 #include <ctype.h>
+#include "chat_parser.h" //for chat parsing
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
 
-#define CLIENT_PORT 10000
+
+
+//Client and server deatils shared between the 2 threads
+int client_sd;//socket descriptor
+struct sockaddr_in server_addr;//server address with IP and the PORT
+char log_filename[64];//
+volatile int keep_running = 1;//client running or shut down?
+
+
+// Threads
+
+
+//the listening thread from the server
+void *receive_thread(void *arg)
+{
+
+}
+
+
+
+
+//The thread sending information to the server
+void *input_thread(void *arg)
+{
+
+}
+
+
+
 
 // client code
 int main(int argc, char *argv[])
 {
-    // This function opens a UDP socket,
-    // binding it to all IP interfaces of this machine,
-    // and port number CLIENT_PORT.
-    // (See details of the function in udp.h)
-    int sd = udp_socket_open(0);
+    int admin_mode = (argc > 1 && strcmp(argv[1], "admin") == 0);
 
-    /* Retrieve and display the client's bound port number */
-    struct sockaddr_in local_addr;
-    socklen_t local_len = sizeof(local_addr);
-    if (getsockname(sd, (struct sockaddr *)&local_addr, &local_len) == 0)
-    {
-        printf("Client bound to port %d\n", ntohs(local_addr.sin_port));
-    }
-    else
-    {
-        perror("getsockname");
-    }
-    // Variable to store the server's IP address and port
-    // (i.e. the server we are trying to contact).
-    // Generally, it is possible for the responder to be
-    // different from the server requested.
-    // Although, in our case the responder will
-    // always be the same as the server.
-    struct sockaddr_in server_addr, responder_addr;
-
-    // Initializing the server's address.
-    // We are currently running the server on localhost (127.0.0.1).
-    // You can change this to a different IP address
-    // when running the server on a different machine.
-    // (See details of the function in udp.h)
-    int rc = set_socket_addr(&server_addr, "127.0.0.1", SERVER_PORT);
-    while(1){
-        // Storage for request and response messages
-        char client_request[BUFFER_SIZE], server_response[BUFFER_SIZE];
-
-        printf(">> ");
-        fgets(client_request, BUFFER_SIZE, stdin);
-        // Demo code (remove later)
-        //strcpy(client_request, "hello");
-
-        // This function writes to the server (sends request)
-        // through the socket at sd.
-        // (See details of the function in udp.h)
-        rc = udp_socket_write(sd, &server_addr, client_request, BUFFER_SIZE);
-
-        if (rc > 0)
-        {
-            // This function reads the response from the server
-            // through the socket at sd.
-            // In our case, responder_addr will simply be
-            // the same as server_addr.
-            // (See details of the function in udp.h)
-            int rc = udp_socket_read(sd, &responder_addr, server_response, BUFFER_SIZE);
-
-            // Demo code (remove later)
-            printf("server_response: %s", server_response);
-        }
+    //opening a UDP socket
+    if (admin_mode) {
+        //the admin mode uses port 6666
+        client_sd = udp_socket_open(6666);
+        printf("Admin client started on port 6666\n");
+    } else {
+        // normal cline so use any available port
+        client_sd = udp_socket_open(0);
     }
 
+    if (client_sd < 0) {
+        perror("udp_socket_open failed");
+        return 1;
+    }
+
+    //getting the local port we were assigned
+    struct sockaddr_in self_addr;
+    socklen_t addr_len = sizeof(self_addr);
+    if (getsockname(client_sd, (struct sockaddr *)&self_addr, &addr_len) != 0) {
+        perror("getsocket name failed");
+        return 1;
+    }
+    unsigned short my_port = ntohs(self_addr.sin_port);
+    printf("Client bound to UDP port %hu\n", my_port);
+
+
+
+    //set the socket address of server
+    if (set_socket_addr(&server_addr, "127.0.0.1", SERVER_PORT) != 0) {
+        fprintf(stderr, "failed to initialise server address\n");
+        return 1;
+    }
+
+    //building the filename for the current chat
+    snprintf(log_filename, sizeof(log_filename), "iChat_%hu.txt", my_port);
+    printf("Logging chat to %s\n", log_filename);
+
+
+    //after all the setup is done we create the input and reciever threads
+    pthread_t input_tid, recv_tid;
+    pthread_create(&input_tid, NULL, input_thread, NULL);
+    pthread_create(&recv_tid, NULL, receive_thread, NULL);
+
+
+    //waiting for client to stop sending messages to server
+    pthread_join(input_tid, NULL);
+
+
+    //closing the reciever as client stops running
+    keep_running = 0;
+    pthread_join(recv_tid, NULL);
+
+
+    close(client_sd);
     return 0;
 }
+
+
+
+
+
